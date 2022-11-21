39862e1b498056f5cd611101da3014aa40795a1c
maks <maximaa2001@gmail.com>
2022-11-22 01:52:43 +0300

    without z buffer

--- src/main/java/by/bsuir/akg/service/DrawService.java
+++ src/main/java/by/bsuir/akg/service/DrawService.java
@@ -5,9 +5,7 @@ import by.bsuir.akg.constant.Const;
 import by.bsuir.akg.entity.Vector;
 
 import java.awt.*;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class DrawService {
     private static DrawService instance;
@@ -36,9 +34,7 @@ public class DrawService {
         drawDda(p1.getX().intValue(), p1.getY().intValue(), p2.getX().intValue(), p2.getY().intValue(), intens);
         drawDda(p2.getX().intValue(), p2.getY().intValue(), p3.getX().intValue(), p3.getY().intValue(), intens);
         drawDda(p1.getX().intValue(), p1.getY().intValue(), p3.getX().intValue(), p3.getY().intValue(), intens);
-         fillTriangle(p1, p2, p3, intens);
-        // fill(p1, p2, p3, intens);
-        //  rasterTriangle(p1, p2, p3, intens);
+        fillTriangle(p1, p2, p3, intens);
     }
 
     public void drawDda(int x1, int y1, int x2, int y2, float intens) {
@@ -73,106 +69,6 @@ public class DrawService {
         renderController.getBufferedImage().setRGB(x, y, color.getRGB());
     }
 
-    private void fill(Vector dot1, Vector dot2, Vector dot3, float intens) {
-        if (dot1.getY() < 0 || dot2.getY() < 0 || dot3.getY() < 0 || dot1.getX() < 0 || dot2.getX() < 0 || dot3.getX() < 0) {
-            return;
-        }
-        if (dot1.getY() > Const.HEIGHT || dot2.getY() > Const.HEIGHT || dot3.getY() > Const.HEIGHT ||
-                dot1.getX() > Const.WIDTH || dot2.getX() > Const.WIDTH || dot3.getX() > Const.WIDTH) {
-            return;
-        }
-        double xl, xr, zl, zr;
-
-        for (int Y = dot1.getY().intValue(); Y <= dot2.getY().intValue(); Y++) {
-            xl = dot1.x + (dot2.x - dot1.x) * ((Y - dot1.y) / (dot2.y - dot1.y));
-            xr = dot1.x + (dot3.x - dot1.x) * ((Y - dot1.y) / (dot3.y - dot1.y));
-            zl = dot1.z + (dot2.z - dot1.z) * ((Y - dot1.y) / (dot2.y - dot1.y));
-            zr = dot1.z + (dot3.z - dot1.z) * ((Y - dot1.y) / (dot3.y - dot1.y));
-            fillLine(xl, xr, zl, zr, Y, intens);
-        }
-
-        for (int Y = dot2.getY().intValue(); Y <= dot2.getY().intValue(); Y++) {
-            xl = dot2.x + (dot3.x - dot2.x) * ((Y - dot2.y) / (dot3.y - dot2.y));
-            xr = dot1.x + (dot3.x - dot1.x) * ((Y - dot1.y) / (dot3.y - dot1.y));
-            zl = dot2.z + (dot3.z - dot2.z) * ((Y - dot2.y) / (dot3.y - dot2.y));
-            zr = dot1.z + (dot3.z - dot1.z) * ((Y - dot1.y) / (dot3.y - dot1.y));
-            fillLine(xl, xr, zl, zr, Y, intens);
-        }
-    }
-
-
-    public void rasterTriangle(Vector t0, Vector t1, Vector t2, float intens) {
-        if (Objects.equals(t0.y, t1.y) && Objects.equals(t0.y, t2.y)) return; // i dont care about degenerate triangles
-        if (t0.y>t1.y) {
-            Double temp = t0.y;
-            t0.y = t1.y;
-            t1.y = temp;
-        }
-        if (t0.y>t2.y) {
-            Double temp = t0.y;
-            t0.y = t2.y;
-            t2.y = temp;
-        }
-        if (t1.y>t2.y) {
-            Double temp = t1.y;
-            t1.y = t2.y;
-            t2.y = temp;
-        }
-        double total_height = t2.y-t0.y;
-        for (int i=0; i<total_height; i++) {
-            boolean second_half = i>t1.y-t0.y || t1.y.equals(t0.y);
-            double segment_height = second_half ? t2.y-t1.y : t1.y-t0.y;
-            float alpha = (float)(i/total_height);
-            float beta  = (float)((i-(second_half ? t1.y-t0.y : 0))/segment_height); // be careful: with above conditions no division by zero here
-            Vector A = add(t0, scalarMult(minus(t2, t0), alpha));
-            Vector B = second_half ? add(t1, scalarMult(minus(t2, t1), beta)): add(t0, scalarMult(minus(t1, t0), beta));
-            if (A.x>B.x) {
-                Vector temp = A;
-                A = B;
-                B = temp;
-            }
-            for (int j=A.x.intValue(); j<=B.x; j++) {
-                double phi = B.x.equals(A.x) ? 1. : (float)(j-A.x)/(float)(B.x-A.x);
-                Vector P = add(A,scalarMult(minus(B,A), phi));
-                int idx = (int) (P.x+P.y*Const.WIDTH);
-                if (zBuffer[idx]<P.z) {
-                    zBuffer[idx] = P.z;
-                    drawPixel(P.x.intValue(), P.y.intValue(), intens, intens, intens);
-                }
-            }
-        }
-    }
-
-    private Vector add(Vector vector1, Vector vector2) {
-        return new Vector(vector1.getX() + vector2.getX(), vector1.getY() + vector2.getY(), vector1.getZ() + vector2.getZ());
-    }
-
-    private Vector minus(Vector vector1, Vector vector2) {
-        return new Vector(vector1.getX() - vector2.getX(), vector1.getY() - vector2.getY(), vector1.getZ() - vector2.getZ());
-    }
-
-
-    private void fillLine(Double _xl, Double _xr, Double zl, Double zr, int y, float intens) {
-        var xl = _xl;
-        var xr = _xr;
-        if (xl > xr) {
-            var t = xr;
-            xr = xl;
-            xl = t;
-        }
-
-        for (int X = (int) Math.round(xl); X <= (int) Math.round(xr); X++)
-            if (X >= 0 && X < Const.WIDTH && y >= 0 && y < Const.HEIGHT) {
-                var nz = zl + (zr - zl) * ((X - xl) / (xr - xl));
-//                if (zBuffer[y][X] == null || zBuffer[y][X] > nz) {
-//                    //var color = plane.getColor(X.toFloat(), Y.toFloat())
-//                    drawPixel(X, y, intens, intens, intens);
-//                    zBuffer[y][X] = nz;
-//                }
-            }
-
-    }
-
     private void fillTriangle(Vector p1, Vector p2, Vector p3, float intens) {
         Vector topLeft = topLeft(p1, p2, p3);
         Vector bottomRight = bottomRight(p1, p2, p3);
@@ -200,10 +96,6 @@ public class DrawService {
         int bSide = ((p2.getY().intValue() - p3.getY().intValue()) * p.getX().intValue() + (p3.getX().intValue() - p2.getX().intValue()) * p.getY().intValue() + (p2.getX().intValue() * p3.getY().intValue() - p3.getX().intValue() * p2.getY().intValue()));
         int cSide = ((p3.getY().intValue() - p1.getY().intValue()) * p.getX().intValue() + (p1.getX().intValue() - p3.getX().intValue()) * p.getY().intValue() + (p3.getX().intValue() * p1.getY().intValue() - p1.getX().intValue() * p3.getY().intValue()));
         return (aSide >= 0 && bSide >= 0 && cSide >= 0) || (aSide < 0 && bSide < 0 && cSide < 0);
-    }
-
-    private Vector scalarMult(Vector vector1, double alpha) {
-        return new Vector(vector1.getX() * alpha, vector1.getY() * alpha, vector1.getZ() * alpha);
     }
 
     private Vector topLeft(Vector p1, Vector p2, Vector p3) {
